053612e6fd6463d79de11657c243fa4016313c4d
jacobmann <gothub@h-mail.us>
2021-10-08 19:05:32 -0700

    added to requirements doc

--- machine-learning/model-building/501_causal_impact_analysis.py
+++ machine-learning/model-building/501_causal_impact_analysis.py
@@ -1,15 +1,17 @@
+########################################
+# Causal Impact Analysis
+########################################
+
+# %%
 import os 
 
-from causalimpact import CausalImpact
 import pandas as pd
+from causalimpact import CausalImpact
 from sqlalchemy import create_engine
-
-from sklearn.preprocessing import OneHotEncoder
 
 ########################################
 # %% # Read in and shuffle Data
 ########################################
-
 USER_LOGIN = 'postgres' 
 USER_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 SERVICE_NAME = 'localhost'
@@ -19,19 +21,28 @@ DATABASE = 'postgres'
 conn_url = f"postgresql+psycopg2://{USER_LOGIN}:{USER_PASSWORD}@{SERVICE_NAME}:{PORT}/{DATABASE}"
 engine = create_engine(conn_url)
 
-query = """with t as (
+query = """with customer_sales as (
     select 
-        customer_id,
-        transaction_date,
-        sum(sales_cost) as sales_cost
-    from groceries.transactions
-    group by customer_id,
-        transaction_date
+        ct.customer_id,
+        cc.signup_flag,
+        t.transaction_date,
+        sum(t.sales_cost) as sales_cost
+    from groceries.transaction_details t
+    join groceries.customer_transactions ct on t.transaction_id = cc.transaction_id,
+    join groceries.customer_campaign cc on t.customer_id = cc.customer_id
+    group by ct.customer_id,
+        t.transaction_date,
+        cc.signup_flag
 )
 select
-
-from 
-
+    transaction_date,
+    sum(case when not signup_flag then sales_cost else 0) / sum(case when not signup_flag then 1 else 0) as not_signed_up_sales,
+    sum(case when signup_flag then sales_cost else 0) / sum(case when signup_flag then 1 else 0) as signed_up_sales
+from customer_sales
+group by
+    transaction_date
 """
 df = pd.read_sql_query(query, con=engine)
 
+
+# %%
